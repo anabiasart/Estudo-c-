066f5aad6e34c0ebfbfb69e7f2cafd04e2848bf2
ana <akirayang727@gmail.com>
2026-08-07 14:24:30 -0300

    add exerc igual

---
+++ igual/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class somagual {
+static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o primeiro número: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Digite o segundo número: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        if (num1 == 30 && num2 == 30)
+        {
+            Console.WriteLine("Numero verdadeiro a 30");
+        }else if(num1 +num2 == 30)
+        {
+            Console.WriteLine("A soma é igual a 30: Verdadeiro");
+        }
+        else
+        {
+            Console.WriteLine("A soma não é igual a 30: Falso");
+        }
+     
+}
+
+}
