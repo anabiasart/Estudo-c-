6bdca5f06354114aba6e6f87e1a115400835d2ed
ana <akirayang727@gmail.com>
2026-08-07 14:36:31 -0300

    add verificacao de num em 10

---
+++ Verificar num/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace verificar{
+
+    class Program
+{
+    static void Main(string [] args)
+        {
+            Console.WriteLine("Digite um numero inteiro:");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num + 10 ==100 || num +10 == 200)
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+        }
+}
+}
