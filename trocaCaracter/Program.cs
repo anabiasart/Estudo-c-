bc5b271d64e5fe733e823cceff067878b5b6d810
ana <akirayang727@gmail.com>
2026-08-07 18:29:04 -0300

    add exerc troca caracter

---
+++ trocaCaracter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace troca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma palavra para fazermos a troca:");
+            string palavra = Console.ReadLine();
+
+            // Chama o método test e armazena o resultado
+            string resultado = test(palavra);
+
+            Console.WriteLine($"Resultado: {resultado}");
+        }
+
+        public static string test(string str)
+        {
+            // Se a palavra tiver mais de 1 caractere, troca o primeiro com o último
+            return str.Length > 1
+                ? str.Substring(str.Length - 1) + str.Substring(1, str.Length - 2) + str.Substring(0, 1)
+                : str;
+        }
+    }
+}
