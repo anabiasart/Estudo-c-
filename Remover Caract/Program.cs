17711021c95e892ffbb0842b42b923edd42ea7ce
ana <akirayang727@gmail.com>
2026-08-07 15:05:38 -0300

    add exer remover o primeiro caracter

---
+++ Remover Caract/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace caracter {
+    class Program
+{
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma string:");
+            string input = Console.ReadLine();
+
+            string test = input.Remove(0, 1);
+
+            Console.WriteLine("A string após remover o primeiro caractere é: " + test);
+
+}
+}}
