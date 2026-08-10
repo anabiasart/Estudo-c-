22a6ae383f57f9cf03350166fc630383cbd2b964
ana <akirayang727@gmail.com>
2026-08-10 13:36:36 -0300

    add exerc numero inteiro multipo de 7 ou de 3

---
+++ multiplo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace multiplo
+{
+    //verificar se um numero  positivo e multiplo de 3
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero positivo: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            if (numero % 3 == 0 || numero % 7 == 0)
+            {
+                Console.WriteLine("O numero " + numero + " é verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("O numero " + numero + " falso");
+            }
+            {
+                
+            }
+        }
+    }
+}
