51e5367b1e90cd66dac5c953b2f4ede3daff3c2f
ana <akirayang727@gmail.com>
2026-08-10 15:52:03 -0300

    add exerc verso

---
+++ frente&verso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Console.WriteLine("digite uma string por gentileza:");
+string input = Console.ReadLine();
+
+if (input.Length < 3)
+{
+    Console.WriteLine("A string fornecida é menor que 3 caracteres.");
+    return;
+}
+else
+{
+    Console.WriteLine("A string fornecida é: " + input);
+
+    string front = input.Substring(0, 3) + input.Substring(0, 3);
+    string valor = input.Substring(input.Length - 3) + input.Substring(input.Length - 3);
+    string result = front + valor + front;
+    Console.WriteLine("Resultado: " + result);
+
+
+}
