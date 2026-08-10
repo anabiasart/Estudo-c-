9085978496e2790316f309987ad7a19cb1c94f05
ana <akirayang727@gmail.com>
2026-08-10 12:51:53 -0300

    add exerc ultimo caracter rept inicio e fim

---
+++ ultimo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+//criar uma string com o ultimo caracter adicionado no inicio e no final de uma string dada
+//de comprimento igual ou superior a 1
+namespace caracter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma string de comprimento igual ou superior a 1: ");
+            string input = Console.ReadLine();
+
+            if (input.Length >= 1)
+            {
+                char LastChar = input[input.Length - 1];
+                string result = LastChar + input + LastChar;
+                Console.WriteLine("Resultado: " + result);
+
+            }else
+            {
+                Console.WriteLine("A string deve ter comprimento igual ou superior a 1.");
+            }
+        }
+    }
+}
