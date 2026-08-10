2aeeed561f8a8b3cdb5b699830d37b3e85189595
ana <akirayang727@gmail.com>
2026-08-10 16:18:34 -0300

    add exerc comeco

---
+++ comeco/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+//Verificar se uma string comeca com C#
+namespace comeco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma string: ");
+            string str = Console.ReadLine();
+
+            if (str.StartsWith("C#") || str.StartsWith("c#"))
+            {
+                Console.WriteLine("Verdadeiro");
+            }
+            else
+            {
+                Console.WriteLine("Falso");
+            }
+
+        }
+    }
+}
